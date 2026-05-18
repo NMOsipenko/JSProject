1c28f6d6f44b19614e5731b2b3a239e1ce62f11c
Nikolay <NMOsipenko@mail.ru>
2026-05-18 16:22:39 +0300

    Lesson 18

--- js/script.js
+++ js/script.js
@@ -1,22 +1,41 @@
 "use strict"
-console.log(1)
-let wars = 10;
-console.log(wars)
+// console.log(1)
+// let wars = 10;
+// console.log(wars)
 
-let result = confirm('Are your here?');
+// let result = confirm('Are your here?');
 
-alert(result);
-console.log(result);
+// alert(result);
+// console.log(result);
 
-// const answer  = prompt("Вам есть 18 лет?", "18");
-// alert(answer); 
+// // const answer  = prompt("Вам есть 18 лет?", "18");
+// // alert(answer); 
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('Как Вашей имя?', '');
-answers[1] = prompt('Как Ваша фамилия?', '');
-answers[2] = prompt('Сколько Вам лет?', '');
+// answers[0] = prompt('Как Вашей имя?', '');
+// answers[1] = prompt('Как Ваша фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
 
-document.write(answers)
+// document.write(answers)
 
-console.log(typeof(answers));
+// console.log(typeof(answers));
+
+const numbersfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '0');
+
+const personalMivieDB = {
+    count: numbersfFilms,
+    movies: {}, 
+    actors: {}, 
+    gentres: [],
+    privat: false
+};
+
+for (let  i = 0; i < 2 ; i++) {
+    const film = prompt('Какой фильм Вы посмотрели?', '');
+    const rating = +prompt('Какой рейтинг?', '0');
+
+    personalMivieDB.movies[film] = rating;
+}
+
+console.log(personalMivieDB);
