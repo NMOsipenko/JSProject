433881f250cf490e2e2b39889857943016f8d335
Nikolay <NMOsipenko@mail.ru>
2026-05-21 18:03:30 +0300

    Lesson 28

--- js/script.js
+++ js/script.js
@@ -21,7 +21,47 @@
 
 // console.log(typeof(answers));
 
-const numbersfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '0');
+
+// let num = 10;
+
+// //DO-WHILE
+// do {
+//     console.log(num);
+//     num++;
+// } while (num < 16);
+
+// let result = 100;
+
+// for(let i = 0 ; i <= result; i++ ) {
+//     let str = ""
+//     for(let j = 0 ; j <= i; j++ ){
+//         str += "*" 
+//     } 
+//     console.log(str);
+        
+// }
+
+// // Метка Цикла
+// first: for(let i = 0 ; i <= 5; i++){
+//     console.log(`First level ${i}`);
+//     second: for(let j = 0 ; j <= 5; j++) {
+//         console.log(`Second level ${j}`);
+//         thrid: for(let k = 0 ; k <= 5; k++) {
+//             if( k == 2) continue first;
+//             console.log(`Thrid level ${k}`);
+//         }
+//     }  
+// }
+
+let numbersfFilms = '';
+
+function start() {
+   
+    while(numbersfFilms == '' || numbersfFilms == null || isNaN(numbersfFilms))
+        numbersfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '');
+}
+
+start();
 
 const personalMivieDB = {
     count: numbersfFilms,
@@ -31,69 +71,58 @@ const personalMivieDB = {
     privat: false
 }; 
 
-for (let  i = 0; i < 2 ; i++) {
-    let film;
+function rememberMyFilms() {
+    for (let  i = 0; i < 2 ; i++) {
+        let film;
 
-    let invalidAnswer = true;
-    let filmLength = 0;
-    
-    while(invalidAnswer){
-        film = prompt(`Какой фильм Вы посмотрели? Вы ввели ${filmLength} символов.`, '');
-        if(film.length > 0 && film.length <= 50){
-            invalidAnswer = false;
-        }
-        filmLength = film.length;
-    }
-    const rating = +prompt('Какой рейтинг?', '0');
-
-
-    personalMivieDB.movies[film] = rating;
-}
-
-console.log(personalMivieDB);
-
-(numbersfFilms > 10)? console.log(`Вы посмотрели много фильмов ${numbersfFilms}`) : console.log(`Вы посмотрели мало фильмов ${numbersfFilms}`);
-
-switch (numbersfFilms) {
-    case 10:
-        console.log('мало');
-        break;
-    case 20:
-        console.log('много')
-        break;
-    default:
-        console.log('Понятия не имею много это или мало');
-        break;
-
-}
-
-let num = 10;
-
-//DO-WHILE
-do {
-    console.log(num);
-    num++;
-} while (num < 16);
-
-let result = 100;
-
-for(let i = 0 ; i <= result; i++ ) {
-    let str = ""
-    for(let j = 0 ; j <= i; j++ ){
-        str += "*" 
-    } 
-    console.log(str);
+        let invalidAnswer = true;
+        let filmLength = 0;
         
+        while(invalidAnswer){
+            film = prompt(`Какой фильм Вы посмотрели? Вы ввели ${filmLength} символов.`, '');
+            if(film.length > 0 && film.length <= 50){
+                invalidAnswer = false;
+            }
+            filmLength = film.length;
+        }
+        const rating = +prompt('Какой рейтинг?', '0');
+
+
+        personalMivieDB.movies[film] = rating;
+    }
 }
 
-// Метка Цикла
-first: for(let i = 0 ; i <= 5; i++){
-    console.log(`First level ${i}`);
-    second: for(let j = 0 ; j <= 5; j++) {
-        console.log(`Second level ${j}`);
-        thrid: for(let k = 0 ; k <= 5; k++) {
-            if( k == 2) continue first;
-            console.log(`Thrid level ${k}`);
-        }
-    }  
+rememberMyFilms();
+
+function detectedPersonalLevel(l) {
+    (numbersfFilms > 10)? console.log(`Вы посмотрели много фильмов ${numbersfFilms}`) : console.log(`Вы посмотрели мало фильмов ${numbersfFilms}`);
+
+    switch (numbersfFilms) {
+        case 10:
+            console.log('мало');
+            break;
+        case 20:
+            console.log('много')
+            break;
+        default:
+            console.log('Понятия не имею много это или мало');
+            break;
+    }
 }
+
+detectedPersonalLevel();
+
+function showMyDb(){
+    if(!personalMivieDB.privat)
+        console.log(personalMivieDB)
+}
+
+function writeYourGenres(){
+    for(let i = 1; i < 4; i++){
+        const genre = prompt(`Жанр под номером ${i}:`, '');
+        personalMivieDB.gentres[(i-1)] = genre;
+    }
+}
+
+writeYourGenres();
+showMyDb();
